727e45f4fb5d091b32786be7ec3360c21b9753e8
UAAUAGUGA <jinyf16@mails.tsinghua.edu.cn>
2020-08-24 17:13:12 +0800

    Fix

--- bin/run-expression-cancer-NC.sh
+++ bin/run-expression-cancer-NC.sh
@@ -3,7 +3,7 @@ labels=metadata/covariates.txt
 data=expression
 input=variations/${data}.txt
 for trend in up both both_balance;do
-for cancer in LUAD HCC;do
+for cancer in CRC STAD LUAD HCC ESCA;do
 outdir=output-shuffle/${data}/${cancer}-${trend}-NC
 [ -d ${outdir} ] || mkdir -p ${outdir}
 bsub -q Z-LU "python  -W ignore bin/classification.py --input ${input} --labels ${labels} --pos ${cancer} --neg NC --model  ${outdir}/model.pickle --performance ${outdir}/performance.txt --roc ${outdir}/roc.txt --features ${outdir}/features.txt --trend ${trend} > log/${trend}-${data}-${cancer}-NC.txt 2>&1 "
